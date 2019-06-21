b13282d86d6d6eedc6835dad7faf1286bc2b42d9
Kryvtsov15 <45894407+Kryvtsov15@users.noreply.github.com>
2019-06-21 17:17:47 +0300

    Django task

--- test_db2/settings.py
+++ test_db2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!m0db51)jn3gbvtpzdc81yo@%^1(7v2kl)cxkm_(hd1h*(a(f0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,7 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 REST_FRAMEWORK = {
 
